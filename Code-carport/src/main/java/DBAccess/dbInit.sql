e81308759a434d717b5b6be8ddd36bb679ed8822
Mads Kristensen <madsarke@gmail.com>
2020-04-28 20:56:42 +0200

    Extra på databasen

--- Code-carport/src/main/java/DBAccess/dbInit.sql
+++ Code-carport/src/main/java/DBAccess/dbInit.sql
@@ -56,3 +56,21 @@ insert into RoofType value
  (4, "Plastik - sort"),
  (5, "Plastik - Hvid"),
  (6, "Plastik - Træ");
+
+  create table Materials (
+ `materialID` int not null auto_increment,
+ `materialType` varchar(30) not null,
+ `materialDimension` int not null,
+ `materialDetail` varchar(100) not null,
+ `price` double not null,
+ primary key (`materialID`)
+ )ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+ insert into Materials value
+(1, "Stolpe", 100100, "Det er en stolpe som bestemmer højden, trykimpregneret", 39.0),
+(2, "Rem", 045145, "Vandret planke som forbinder stolper, spærtræ", 36.38),
+(3, "Spær", 045095, "Holder de to sider sammen, reglar.", 23.20),
+(4, "Stern", 025150, "Det er rammen på porten, trykimpregneret", 39.0),
+(5, "Søm", 0, "Holder sammen på lortet", 230.0),
+(6, "Skruer", 0, "Holder sammen på lortet", 130.0),
+(7, "Hulbånd", 0, "Holder to ting sammen", 559.0);
