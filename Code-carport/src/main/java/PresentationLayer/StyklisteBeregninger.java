ac41ab6180cdb044a5614131bceb16d9b557057d
jonasmik <jonassoldath@live.dk>
2020-05-07 15:38:30 +0200

    pepega calculations

--- Code-carport/src/main/java/PresentationLayer/StyklisteBeregninger.java
+++ Code-carport/src/main/java/PresentationLayer/StyklisteBeregninger.java
@@ -2,6 +2,7 @@ package PresentationLayer;
 
 
 import DBAccess.MaterialsMapper;
+import FunctionLayer.materials;
 
 import java.util.HashMap;
 
@@ -64,25 +65,46 @@ public class StyklisteBeregninger {
 
     }
 
-    public static int calculateStolper(int length, int width, int height){
+    public static int calculateStolper(int length, int width, int height) {
         HashMap<String, FunctionLayer.materials> materialer = MaterialsMapper.hashMapAfMaterialer();
-        int stolper = 4;
+        int stolperAntalWidth = 2;
+        int stolperAntalLength = 2;
 
 
-        if (length > 420 && length <= 630){
-            stolper += 2;
+        if (length > 420 && length <= 630) {
+            stolperAntalLength += 1;
 
-        }else if (length > 630){
-            stolper += 4;
+        } else if (length > 630) {
+            stolperAntalLength += 2;
         }
-        if (width > 520){
-            stolper += 2;
+        if (width > 520) {
+            stolperAntalWidth += 1;
         }
-        
+        int antalStolper = stolperAntalLength*stolperAntalWidth;
+        int cmStolpe = height*antalStolper;
 
+        materials stolpeLength1 = materialer.get("Trykimpregneret stolpe1");
+        materials stolpeLength2 = materialer.get("Trykimpregneret stolpe43");
+
+        int stl1 = stolpeLength1.getMatLength();
+        int stl2 = stolpeLength2.getMatLength();
+
+        int stolpeMath1 = cmStolpe/stl1;
+        int stolpeExtra1 = cmStolpe%stl1;
+
+        int stolpeMath2 = stolpeExtra1/stl2;
+        int stolpeExtra2 = stolpeExtra1%stl2;
+
+        if (stolpeExtra2 > 0){
+            stolpeMath2 +=1;
+        }
+
+
+        int totalStolpe = stolpeMath1+stolpeMath2;
+
+        return totalStolpe;
     }
-
-
+        
 
 }
 
