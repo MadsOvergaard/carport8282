a786ab2d755a50afb33bbebb9f850cf670fa8f76
jonasmik <jonassoldath@live.dk>
2020-05-11 12:59:47 +0200

    Spær beregninger

--- Code-carport/src/main/java/PresentationLayer/StyklisteBeregninger.java
+++ Code-carport/src/main/java/PresentationLayer/StyklisteBeregninger.java
@@ -19,6 +19,7 @@ public class StyklisteBeregninger {
     private static int stolperAntalLength = 2;
     private static int stolperAntalWidth = 2;
 
+
     //Udregner Længden af Losholter
     public static double CalculateLosholterSide(int shackLength) {
         LosholterSide = (shackLength - (2 * 9.7)) * 2;
@@ -37,7 +38,7 @@ public class StyklisteBeregninger {
     //Udregner Længden af det skrå tag
     public static double CalculateVinskederLength(int width, int slopeAngle) {
         double temp = (width / 2);
-        VinskederLength = 2 * Math.sqrt(Math.pow((width / 2) * Math.tan(slopeAngle), 2.0) + pow(temp, 2));
+        VinskederLength = 2 * Math.sqrt(Math.pow((temp) * Math.tan(slopeAngle), 2.0) + pow(temp, 2));
 
         return VinskederLength;
 
@@ -132,10 +133,35 @@ public class StyklisteBeregninger {
         return remAntal1 + " " + remAntal2;
     }
 
-    public static String calculateSpaer(int length, int width) {
+    public static String calculateSpaerFladtTag(int length, int width) {
         HashMap<String, FunctionLayer.materials> materialer = MaterialsMapper.hashMapAfMaterialer();
         int spaerAntalRaekker = length / 80; // 80 er den længde der skal være mellem spær
         int maxSpaerLength = spaerAntalRaekker * width;
+
+        materials spaer1 = materialer.get("Spærtræ2");
+        materials spaer2 = materialer.get("Spærtræ3");
+        int spaer1Length = spaer1.getMatLength();
+        int spaer2Length = spaer2.getMatLength();
+
+        int spaerMath1 = maxSpaerLength / spaer1Length;
+        int spaerExtra1 = maxSpaerLength % spaer1Length;
+        int spaerMath2 = spaerExtra1 / spaer2Length;
+        int spaerExtra2 = spaerExtra1 % spaer2Length;
+
+        if (spaerExtra2 > 0) {
+            spaerMath2 += 1;
+        }
+
+        String spaerAntal1 = spaerMath1 + " " + spaer1.getMatLength() + " " + spaer1.getMatType();
+        String spaerAntal2 = spaerMath2 + " " + spaer2.getMatLength() + " " + spaer2.getMatType();
+        return spaerAntal1 + " " + spaerAntal2;
+    }
+
+    public static String calculateSpaerRejsning(int length, int width, int slopeAngle) {
+        HashMap<String, FunctionLayer.materials> materialer = MaterialsMapper.hashMapAfMaterialer();
+        int spaerAntalRaekker = length / 80; // 80 er den længde der skal være mellem spær
+        double hypotenusenx2 = CalculateVinskederLength(width, slopeAngle);
+        int maxSpaerLength = ((int)hypotenusenx2 + width)*spaerAntalRaekker;
 
         materials spaer1 = materialer.get("Spærtræ2");
         materials spaer2 = materialer.get("Spærtræ3");
